d6c573a623af2bc32d8e17d0af6a795b4e61bc6a
NikeBoy128 <haroldmosquera739@gmail.com>
2024-08-16 12:14:59 -0500

    chore: Update AWS SDK version and add file upload and delete functionality

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,13 @@
 import { Module, DynamicModule } from '@nestjs/common';
 import { SharedUserRepository } from './repositories/sharedUserRepository.repository';
-
-@Module({})
+import { UploadFileService } from './services/uploadFile.service';
+import { S3Service } from './services/s3.service';
+import { UploadFileController } from './controllers/uploadFile.controller';
+import { UploadFileUseCase } from './useCase/uploadFile.useCase';
+@Module({
+  providers: [UploadFileService, S3Service, UploadFileUseCase],
+  controllers: [UploadFileController],
+})
 export class SharedModule {
   static forRoot(): DynamicModule {
     return {
